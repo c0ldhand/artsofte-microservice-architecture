aa5ff2ecd0d4ff900136aafd4a976749b3cfd496
c0ldhand <evgeny_nazarov@inbox.ru>
2025-11-18 00:05:10 +0500

    feat: add semaphore

---
+++ Services/CoreLib/DistributedSemaphore/RedisSemaphoreExtensions.cs
@@ -0,0 +1,51 @@
+﻿using CoreLib.DistributedSemaphore.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace CoreLib.DistributedSemaphore
+{
+    public static class RedisSemaphoreExtensions
+
+
+
+    {
+
+
+        public static IServiceCollection AddRedisDistributedSemaphore(this IServiceCollection services,
+
+
+            string redisConfiguration)
+
+
+        {
+
+
+            if (string.IsNullOrWhiteSpace(redisConfiguration))
+
+
+                throw new ArgumentNullException(nameof(redisConfiguration));
+
+
+
+
+
+            var mux = ConnectionMultiplexer.Connect(redisConfiguration);
+
+
+            services.AddSingleton<IConnectionMultiplexer>(mux);
+
+
+            services.AddSingleton<IDistributedSemaphore, RedisDistributedSemaphore>();
+
+
+
+
+
+            return services;
+
+
+        }
+
+
+    }
+}
---
+++ Services/CoreLib/DistributedSemaphore/SemaphoreLease.cs
@@ -0,0 +1,114 @@
+﻿using CoreLib.DistributedSemaphore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLib.DistributedSemaphore
+{
+    public sealed class SemaphoreLease : IAsyncDisposable, IDisposable
+
+
+
+    {
+
+
+        private readonly IDistributedSemaphore _semaphore;
+
+
+
+
+
+        private int _released;
+
+
+
+
+
+        public SemaphoreLease(IDistributedSemaphore semaphore, string key, string token, int permits = 1)
+
+
+        {
+
+
+            _semaphore = semaphore ?? throw new ArgumentNullException(nameof(semaphore));
+
+
+            Key = key;
+
+
+            Token = token;
+
+
+            Permits = permits;
+
+
+        }
+
+
+
+
+
+        public string Key { get; }
+
+
+        public string Token { get; }
+
+
+        public int Permits { get; }
+
+
+
+
+
+        public ValueTask DisposeAsync()
+
+
+        {
+
+
+            return new ValueTask(ReleaseAsync(CancellationToken.None));
+
+
+        }
+
+
+
+
+
+        public void Dispose()
+
+
+        {
+
+
+            ReleaseAsync(CancellationToken.None).GetAwaiter().GetResult();
+
+
+        }
+
+
+
+
+
+        public Task ReleaseAsync(CancellationToken cancellationToken = default)
+
+
+        {
+
+
+            return Interlocked.Exchange(ref _released, 1) == 1
+
+
+                ? Task.CompletedTask
+
+
+                : _semaphore.ReleaseAsync(Key, Token, cancellationToken);
+
+
+        }
+
+
+    }
+}
